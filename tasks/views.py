6b4261c77dc149e9297085ff30adc7db7dc23e22
Kim Roman <romankim94@gmail.com>
2023-12-29 00:04:01 +0600

    Теперь при создании Task без указания Ñlist будет присваиваться List General

--- tasks/views.py
+++ tasks/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Task
 from .permissions import AdminOrOwnTaskPermission
 from .serializers import TaskListSerializer, TaskDetailSerializer
+from lists.models import List
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskListSerializer
-    permission_classes = (AdminOrOwnTaskPermission, )
+    permission_classes = (IsAuthenticated, AdminOrOwnTaskPermission)
 
     def get_serializer_class(self):
         if self.action != 'list':
@@ -21,3 +24,11 @@ class TaskViewSet(viewsets.ModelViewSet):
         if not self.request.user.is_staff:
             qs = qs.filter(list__person=self.request.user)
         return qs
+
+    def create(self, request, *args, **kwargs):
+        request.data.setdefault('list', List.objects.filter(person=self.request.user, name='General').first().id)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
