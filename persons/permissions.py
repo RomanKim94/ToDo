dc4c7574927941096e71187f4ed35af5a47d04fd
Kim Roman <romankim94@gmail.com>
2023-12-28 21:56:26 +0600

    Теперь при собзез автоÐризации возможно создание модели Person. При создании данной модели автоматически создается Ðмодель List с названием General

--- persons/permissions.py
+++ persons/permissions.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import BasePermission
 class SelfOrAdminPersonPermission(BasePermission):
 
     def has_permission(self, request, view):
-        return request.user and request.user.is_staff or view.action != 'list'
+        return view.action in ['retrieve', 'create'] or request.user and request.user.is_staff
 
     def has_object_permission(self, request, view, obj):
         return bool(obj == request.user or request.user and request.user.is_staff)
--- persons/views.py
+++ persons/views.py
@@ -1,7 +1,10 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
 from .models import Person
 from .permissions import SelfOrAdminPersonPermission
 from .serializers import PersonSelfSerializer, PersonCreateSerializer
+from lists.models import List
 
 
 class PersonViewSet(viewsets.ModelViewSet):
@@ -13,3 +16,11 @@ class PersonViewSet(viewsets.ModelViewSet):
         if self.action == 'create':
             return PersonCreateSerializer
         return PersonSelfSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        List.objects.create(name='General', person=Person.objects.filter(username=serializer.data.get('username')).first())
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
